f8f99af882e435c69b9772b72be1db93498487e0
Alpha-and-Gecc <86210245+Alpha-and-Gecc@users.noreply.github.com>
2023-01-22 22:44:25 -0800

    made walls

--- main.py
+++ main.py
@@ -4,10 +4,104 @@
 
 import pygame as pg
 import numpy as np
+import random
 
+# Creating player
+class Player:
+    # Creation code
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+# Creating enemie
+class Enemy:
+    # Creation code
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+def create_maze(maze_size, start_side, end_side, num_enemies):
+    # Initialize the maze with all walls
+    maze = [[1 for _ in range(maze_size * 2 + 1)] for _ in range(maze_size * 2 + 1)]
+    for x in range(1, maze_size * 2 + 1, 2):
+        for y in range(1, maze_size * 2 + 1, 2):
+            maze[x][y] = 0
+    start_x, start_y = None, None
+    end_x, end_y = None, None
+    if start_side == 'top':
+        start_x, start_y = 1, random.randint(1, maze_size)
+    elif start_side == 'bottom':
+        start_x, start
+    # Start the depth-first search algorithm at the start point
+    stack = [(start_x, start_y)]
+    visited = set()
+    while stack:
+        # Get the current cell from the stack
+        x, y = stack[-1]
+
+        # Mark the current cell as visited
+        visited.add((x, y))
+
+        # Check the neighboring cells
+        neighbors = []
+        if x > 1 and (x - 2, y) not in visited:
+            neighbors.append((x - 2, y))
+        if x < maze_size * 2 - 1 and (x + 2, y) not in visited:
+            neighbors.append((x + 2, y))
+        if y > 1 and (x, y - 2) not in visited:
+            neighbors.append((x, y - 2))
+        if y < maze_size * 2 - 1 and (x, y + 2) not in visited:
+            neighbors.append((x, y + 2))
+
+        # If there are any unvisited neighboring cells, pick one at random
+        # and push it to the stack
+        if neighbors:
+            next_cell = random.choice(neighbors)
+            stack.append(next_cell)
+
+            # Remove the wall between the current cell and the next cell
+            dx, dy = next_cell[0] - x, next_cell[1] - y
+            maze[x + (dx // 2)][y + (dy // 2)] = 0
+        else:
+            # If there are no unvisited neighboring cells, backtrack
+            stack.pop()
+
+    # remove walls between start and end points
+    if start_x == 1:
+        maze[start_x][start_y] = 0
+    elif start_x == maze_size * 2 - 1:
+        maze[start_x][start_y] = 0
+    elif start_y == 1:
+        maze[start_x][start_y] = 0
+    elif start_y == maze_size * 2 - 1:
+        maze[start_x][start_y] = 0
+    if end_x == 1:
+        maze[end_x][end_y] = 0
+    elif end_x == maze_size * 2 - 1:
+        maze[end_x][end_y] = 0
+    elif end_y == 1:
+        maze[end_x][end_y] = 0
+    elif end_y == maze_size * 2 - 1:
+        maze[end_x][end_y] = 0
+
+    # Create player and enemies
+    player = Player(start_x, start_y)
+    enemies = []
+    for i in range(num_enemies):
+        enemy_x, enemy_y = random.randint(1, maze_size * 2 - 1), random.randint(1, maze_size * 2 - 1)
+        while (enemy_x, enemy_y) in visited:
+            enemy_x, enemy_y = random.randint(1, maze_size * 2 - 1), random.randint(1, maze_size * 2 - 1)
+        enemies.append(Enemy(enemy_x, enemy_y))
+
+    # Place player and enemies in the maze
+    maze[player.x][player.y] = 'P'
+    for enemy in enemies:
+        maze[enemy.x][enemy.y] = 'E'
+
+    return maze
 
 #TODO: ADD A WAY TO MOVE THE CAMERA UP AND DOWN
-def main():
+def main(map):
     pg.init()
     window = pg.display.set_mode((1600, 1200))
     running = True
@@ -32,7 +126,11 @@ def main():
     sky = pg.image.load('skybox.jpg')
     sky = pg.surfarray.array3d(pg.transform.scale(sky, (360, halfvres * 2))) / 255
     floor = pg.surfarray.array3d(pg.image.load('floor.jpg')) / 255
+    wall = pg.surfarray.array3d(pg.image.load('floor.jpg')) / 255
     #loads images from the project folder
+
+    while vres < halfvres*2:
+        vres += 10
 
     while running:
         for event in pg.event.get():
@@ -48,6 +146,7 @@ def main():
             #gets the location coordinate of this particular column
             adjustCos = np.cos(np.deg2rad(i/mod - (fov/2)))
             # variable to prevent warping
+            #frame[i][:] == floor[int(np.rad2deg(columnAngle)%99)][:]
 
             for p in range(vres):
                 """for every column of pixels, find the colour of each pixel in said column from the bottom up"""
@@ -64,8 +163,18 @@ def main():
                 shade = 0.1 + 0.1 * (1 - p / (vres/2))
                 #light strength plus max brightness value times the base floor pixel colour
 
-                frame[i][halfvres * 2 - p - 1] = shade*floor[amendedX][amendedY]
-                #renders the pixel at the right colour
+                if map[int(pixelX)%len(map)][int(pixelY%len(map))] == 1:
+                    wallheight = halfvres - p
+                    paint = shade*np.ones(3)
+                    for k in range(wallheight*2):
+                        frame[i][halfvres - wallheight + k] = paint
+                    break
+
+                else:
+                    frame[i][halfvres * 2 - p - 1] = shade*floor[amendedX][amendedY]
+                    #renders the pixel at the right colour
+
+
 
 #        frame = new_frame(posX, posY, rot, frame, hres, halfvres, mod)
         surf = pg.surfarray.make_surface(frame * 255)
@@ -79,7 +188,6 @@ def main():
 
         posX, posY, yaw, vres, halfvres, keys = movement(posX, posY, yaw, vres, halfvres, pg.key.get_pressed())
         #moves the player
-
 
 def movement(posX, posY, yaw, vres, halfvres, keys):
     """fairly simple code to move the player's coordinates"""
@@ -109,6 +217,12 @@ def movement(posX, posY, yaw, vres, halfvres, keys):
     return posX, posY, yaw, vres, halfvres, keys
 
 if __name__ == "__main__":
-    main()
+    size = 5
+    num_enemies = 3
+    map = create_maze(size, 'top', 'bottom', num_enemies)
+    for row in map:
+        print(row)
+
+    main(map)
     pg.quit()
     #runs the game as a function instead of as hard code
